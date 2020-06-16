f1813cc019d149a6326dcbd80fefbe0ea60b10c0
viraltaco <7462994+viraltaco@users.noreply.github.com>
2020-06-16 07:50:00 +0200

    Added: Constructors, destructors, class_name string. Changed: kUnsupportedTerms from std::string_view[3] to std::array<char const*, 4>;          Added nullptr to the list kUnsupportedTerms. Removed: Windows support

--- linenoise/model/ViralNoise.hpp
+++ linenoise/model/ViralNoise.hpp
@@ -6,51 +6,106 @@
 // ┃ SPDX-License-Identifier: MIT                         ┃
 // ┃ <http://www.opensource.org/licenses/MIT>             ┃
 // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-#define VT_VIRALNOISE_HPP "1.0.0"
+#define VT_VIRALNOISE_HPP "2.0.0"
 
-#ifndef _WIN32
-#  include <sys/ioctl.h>
-#  include <termios.h>
-#  include <unistd.h>
-#else
-#  include "win32/winnoise.hpp"
-#endif
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <cstdlib> // std::getenv
 
 #include "Completions.hpp"
 #include <string>
 #include <vector>
+#include <array>
 
 namespace viraltaco_ {
-using std::string_view_literals::operator""sv;
+using namespace std::literals;
 
-struct ViralNoise {
+template <typename StringLikeType> struct ViralNoise {
 public:
+  /// Fully qualified class name
+  static constexpr auto class_name = "viraltaco_::ViralNoise";
   using Self = ViralNoise;
+  using CallbackType = typename Completions<StringLikeType>::CallbackType;
   
 public: // members
-#ifndef _WIN32
   /// In order to restore at exit.
   static termios orig_termios; // defined in unistd.h
-#endif
-  enum maximum : std::size_t { history_len = 100ull, line_length = 4096ull, };
-
-  static constexpr std::string_view kUnsupportedTerms[3] {
-    "dumb"sv, "cons25"sv, "emacs"sv
+  enum maximum : std::size_t {
+    history_len = 100ull,
+    line_length = 4096ull,
   };
   
-  static CompletionCallback completionCallback;
-  ///  For atexit() function to check if restore is needed
-  bool is_raw_mode = false;
-  ///  Multi line mode. Default is single line.
+  static constexpr std::array<char const*, 4> kUnsupportedTerms {
+    "dumb",
+    "cons25",
+    "emacs",
+    nullptr, // Unknown therefore unsupported.
+  };
+  /// Callback function.
+  static CallbackType completion_callback;
+  /// Multi line mode. Default is single line.
   bool allow_multiline = false;
   /// Register atexit just 1 time.
   bool atexit_registered = false;
   /// History length
   std::size_t history_max_len = Self::maximum::history_len;
-
-  Completions history;
+  
+  /// For unsupported terminals
+  bool is_raw_mode;
+  Completions<StringLikeType> history;
+  StringLikeType prompt;
+  
 public: // inits
+  /// No default constructor.
+  ViralNoise() = delete;
+  ViralNoise(Self&&) noexcept = default;
+  ViralNoise(Self const&) noexcept = default;
+  
+  
+  /**
+   * This constructor take a prompt to be displayed on the terminal
+   * and a {@code Completions<>} to add to the otherwise empty
+   * history variable (of the same type) since both behave identically.
+   *
+   * @param prompt the terminal prompt
+   * @param completions the completion strings
+   */
+  ViralNoise(StringLikeType prompt,
+             Completions<StringLikeType> completions)
+    : is_raw_mode{ Self::is_unsupported_term() }
+    , history{ completions }
+    , prompt{ prompt }
+  {}
+  
+  explicit ViralNoise(StringLikeType prompt)
+    : is_raw_mode{ Self::is_unsupported_term() }
+    , prompt { prompt }
+  {}
 
+public: // destructors
+  ~ViralNoise() noexcept {
+    if (is_raw_mode) {
+      (is_raw_mode = tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) != -1);
+    }
+  }
+
+protected: // class methods
+  static bool is_unsupported_term() noexcept {
+    const auto env_term = std::getenv("TERM");
+    /// Return true if the terminal is unsupported
+    return std::any_of(kUnsupportedTerms.begin(), kUnsupportedTerms.end(),
+      [env_term] (const auto term) noexcept -> bool {
+        return term == env_term;
+      }
+    );
+  }
+  
+public: // instance methods
+  
+  template <typename T> Self& operator >> (T& arg_ref) {
+  
+  }
 
 };
 } namespace vt = viraltaco_;
